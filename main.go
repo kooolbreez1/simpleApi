5adb99868611e345ad4a2e0e2d11f52c4ed1dbc7
Brandon Brown <monkeyseemonkeykill42@gmail.com>
2019-02-20 12:01:31 -0500

    simple api for use in kong example

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func YourHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello Root!\n"))
+	fmt.Println("Hello Root!")
+}
+func YourPubHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello Public World!\n"))
+	fmt.Println("Hello World!")
+}
+func YourPrivHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello Private World!\n"))
+	fmt.Println("Hello Private World!")
+}
+
+func main() {
+	r := mux.NewRouter()
+	// Routes consist of a path and a handler function.
+	r.HandleFunc("/", YourHandler)
+	r.HandleFunc("/private", YourPrivHandler)
+	r.HandleFunc("/public", YourPubHandler)
+
+	// Bind to a port and pass our router in
+	log.Fatal(http.ListenAndServe(":3000", r))
+}
